821233f321c08b0178371b13040da4c63941736e
MOzil-10 <mukosibudeli@gmail.comm>
2025-01-20 22:18:16 +0200

    Created a banking app

---
+++ src/main/java/com/banking/api/entity/Token.java
@@ -0,0 +1,4 @@
+package com.banking.api.entity;
+
+public class Token {
+}
---
+++ src/main/java/com/banking/api/entity/TransactionType.java
@@ -0,0 +1,8 @@
+package com.banking.api.entity;
+
+public enum TransactionType {
+    CASH_WITHDRAWAL,
+    CASH_DEPOSIT,
+    CASH_TRANSFER,
+    CASH_CREDIT
+}
